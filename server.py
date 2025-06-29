848c760c504b54aa136731dfdf5a849063093587
Abhishek Chandravanshi <a.chandravanshi.phy20@itbhu.ac.in>
2025-06-29 10:52:44 -0400

    Deployed emotion detection web app using Flask

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/emotionDetector", methods=["POST"])
+def emotion_detection_api():
+    text_to_analyze = request.form['text']
+    result = emotion_detector(text_to_analyze)
+
+    formatted_result = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+    return formatted_result
